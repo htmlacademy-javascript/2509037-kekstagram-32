2c5c187ec6276a3663f9528b2e491e30e897761c
Danil <danil.danya.200296@gmail.com>
2024-07-05 14:41:10 +0300

    module5-task1

---
+++ js/util.js
@@ -0,0 +1,10 @@
+function checkStringLength(str, maxLength) {
+  return str.length <= maxLength;
+}
+
+function isPalindrome(str) {
+  const normalizedStr = str.toLowerCase().replace(/\s/g, '');
+  return normalizedStr === normalizedStr.split('').reverse().join('');
+}
+export {checkStringLength,isPalindrome};
+
