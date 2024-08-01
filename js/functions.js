39e935b71fcc89584fb463d3089c918decc63ce3
Danil <danil.danya.200296@gmail.com>
2024-08-01 12:46:48 +0300

    module5-task2

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,19 @@
+function meetingInWorkingHours (startTime, endTime, meetingStart, meetingDuration) {
+  const [startHour, startMinute] = startTime.split(':').map(Number);
+  const [endHour, endMinute] = endTime.split(':').map(Number);
+  const startMinutes = startHour * 60 + startMinute;
+  const endMinutes = endHour * 60 + endMinute;
+
+  // Преобразуем время начала встречи в минуты
+  const [meetingHour, meetingMinute] = meetingStart.split(':').map(Number);
+  const meetingStartMinutes = meetingHour * 60 + meetingMinute;
+
+  // Вычисляем время окончания встречи в минутах
+  const meetingEndMinutes = meetingStartMinutes + meetingDuration;
+
+  // Проверяем, что встреча начинается и заканчивается в рамках рабочего дня
+  return meetingStartMinutes >= startMinutes && meetingEndMinutes <= endMinutes;
+
+}
+meetingInWorkingHours();
 
